eb023cd7e372e8548131360cf09fb4a513319782
Mateus Silva <mateusilva767@gmail.com>
2023-12-04 15:10:43 -0300

    displaying message of contact not registered and contact not found

--- src/services/ContactsService.js
+++ src/services/ContactsService.js
@@ -1,11 +1,12 @@
-import delay from '../utils/delay';
+import HttpClient from './utils/HttpClient';
 
 class ContactsService {
-  async ListContacts(orderBy = 'asc') {
-    const response = await fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`);
+  constructor() {
+    this.httpClient = new HttpClient('http://localhost:3001');
+  }
 
-    await delay(500);
-    return response.json();
+  async ListContacts(orderBy = 'asc') {
+    return this.httpClient.get(`/contacts?orderBy=${orderBy}`);
   }
 }
 
