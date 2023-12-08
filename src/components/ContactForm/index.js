4837807662d8deaa9b5c2d09766fb257636a2c32
Mateus Silva <mateusilva767@gmail.com>
2023-12-08 13:15:48 -0300

    creating and styling the Toast component

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -18,6 +18,7 @@ export default function ContactForm({ buttonLabel, onSubmit }) {
   const [categoryId, setCategoryId] = useState('');
   const [categories, setCategories] = useState([]);
   const [isLoadingCategories, setIsLoadingCategories] = useState(true);
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const {
     setError, removeError, getErrorMessageByName, erros,
@@ -63,12 +64,16 @@ export default function ContactForm({ buttonLabel, onSubmit }) {
     setPhone(formatPhone(event.target.value));
   }
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
 
-    onSubmit({
+    setIsSubmitting(true);
+
+    await onSubmit({
       name, email, phone, categoryId,
     });
+
+    setIsSubmitting(false);
   }
 
   return (
@@ -79,6 +84,7 @@ export default function ContactForm({ buttonLabel, onSubmit }) {
           placeholder="Nome *"
           value={name}
           onChange={handleNameChange}
+          disabled={isSubmitting}
         />
       </FormGroup>
       <FormGroup error={getErrorMessageByName('email')}>
@@ -88,6 +94,7 @@ export default function ContactForm({ buttonLabel, onSubmit }) {
           placeholder="E-mail"
           value={email}
           onChange={handleEmailChange}
+          disabled={isSubmitting}
         />
       </FormGroup>
       <FormGroup>
@@ -96,6 +103,7 @@ export default function ContactForm({ buttonLabel, onSubmit }) {
           value={phone}
           onChange={handlePhoneChange}
           maxLength="15"
+          disabled={isSubmitting}
         />
       </FormGroup>
 
@@ -103,7 +111,7 @@ export default function ContactForm({ buttonLabel, onSubmit }) {
         <Select
           value={categoryId}
           onChange={(event) => setCategoryId(event.target.value)}
-          disabled={isLoadingCategories}
+          disabled={isLoadingCategories || isSubmitting}
         >
           <option value="">Sem categoria</option>
           {categories.map((category) => (
@@ -116,7 +124,11 @@ export default function ContactForm({ buttonLabel, onSubmit }) {
       </FormGroup>
 
       <ButtonContainer>
-        <Button type="submit" disabled={!isFormValid}>
+        <Button
+          type="submit"
+          disabled={!isFormValid}
+          isLoading={isSubmitting}
+        >
           {buttonLabel}
         </Button>
       </ButtonContainer>
---
+++ src/components/Toast/ToastMessage/index.js
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import { Container } from './styles';
+
+import xCircleIcon from '../../../assets/images/icons/x-circle.svg';
+import checkCircleIcon from '../../../assets/images/icons/check-circle.svg';
+
+export default function ToastContainer({ text, type }) {
+  return (
+    <Container>
+      {type === 'danger' && <img src={xCircleIcon} alt="X" /> }
+      {type === 'success' && <img src={checkCircleIcon} alt="check" /> }
+      <strong>{text}</strong>
+    </Container>
+  );
+}
+
+ToastContainer.propTypes = {
+  text: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(['default', 'success', 'danger']),
+};
+
+ToastContainer.defaultProps = {
+  type: 'default',
+};
